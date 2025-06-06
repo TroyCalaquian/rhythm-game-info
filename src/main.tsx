ee62b50904d3f3d31584220e521417602ca32346
LaggySoul <trcalaquian@gmail.com>
2025-06-05 18:17:08 -0700

    Revert "Install HeroUI packages"
    This reverts commit 0925dd7534fe9de5a94144d6c1c223068cd79042.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
-import {HeroUIProvider} from '@heroui/react'
-import './App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HeroUIProvider>
-      <App />
-    </HeroUIProvider>
+    <App />
   </StrictMode>,
 )

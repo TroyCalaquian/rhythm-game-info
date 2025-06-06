0925dd7534fe9de5a94144d6c1c223068cd79042
LaggySoul <trcalaquian@gmail.com>
2025-06-05 17:59:11 -0700

    Install HeroUI packages

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.tsx'
+import {HeroUIProvider} from '@heroui/react'
+import './App.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
